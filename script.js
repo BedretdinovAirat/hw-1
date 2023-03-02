027cc4e8a040d46cf4187cd035be84528c50cee9
BedretdinovAirat <bedretdinov-a@mail.ru>
2023-03-02 18:21:25 +0300

    02.03.2023

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let salary = Number(prompt('Введите вашу зп'));
-let allMoney = salary / 100 * 87;
-console.log(`Деньги которые в твоём распоряжении ${allMoney}`);
+// let salary = Number(prompt('Введите вашу зп'));
+// let allMoney = salary / 100 * 87;
+// console.log(`Деньги которые в твоём распоряжении ${allMoney}`);
